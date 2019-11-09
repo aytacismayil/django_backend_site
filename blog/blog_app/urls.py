2107bf51d516f2e370876d902dd6a53ba1f34156
aytacismayil <aytac.smayil.97@mail.ru>
2019-11-10 03:11:10 +0400

    django-backend-site-first

---
+++ blog/blog_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',PostListViews.as_view(),name='home'),
+    path('user/<str:username>',UserPostListViews.as_view(),name='user-posts'),
+    path('post/<int:pk>/',PostDetailViews.as_view(),name='post-detail'),
+    path('post/new/',PostCreateViews.as_view(),name='post-create'),
+    path('post/<int:pk>/update/',PostUpdateViews.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/',PostDeleteViews.as_view(),name='post-delete'),
+    path('about/',about,name='about'),
+]
---
+++ blog/blog_app/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render,get_object_or_404
+from .models import *
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.models import User
+
+class PostListViews(ListView):
+	model = Post
+	template_name='home.html'
+	context_object_name='posts'
+	# ordering=['-date_posted']
+	paginate_by=2
+
+
+class UserPostListViews(ListView):
+	model = Post
+	template_name='user_posts.html'
+	context_object_name='posts'
+	ordering=['-date_posted']
+	paginate_by=2
+
+	def get_queryset(self):
+		user=get_object_or_404(User,username=self.kwargs.get("username"))
+		return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+
+class PostDetailViews(DetailView):
+	model = Post
+	template_name='post_detail.html'
+
+class PostCreateViews(LoginRequiredMixin,CreateView):
+	model = Post
+	fields = ['title','content']
+	template_name='post_form.html'
+
+	def form_valid(self,form):
+		form.instance.author =self.request.user
+		return super().form_valid(form)
+
+class PostUpdateViews(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+	model = Post
+	fields = ['title','content']
+	template_name='post_form.html'
+
+	def form_valid(self,form):
+		form.instance.author =self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post=self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteViews(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+	model = Post
+	template_name='post_confirm_delete.html'
+	success_url="/blog/"
+	
+	def test_func(self):
+		post=self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+	
+
+def about(request):
+	return render(request, 'about.html', {'title':"About"})
